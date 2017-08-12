3308013a301487d7224db8bd761d28fbff395dd9
josephm1 <josephmeagher2000@gmail.com>
2017-08-12 15:59:06 +0100

    no longer needs to authorise to view messages
    also refreshs after every message sent

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 "Use strict";
 
 $('#sendmessage').click(function() {
-  sendmessage();
+  authorise();
 });
 $('#refresh').click(function() {
   getMessages();
 });
 
 //initialises and authorises with the network
- (function() {
+(function() {
   var app = {
     name: "Safe Chat",
     id: "joe",
@@ -17,56 +17,92 @@ $('#refresh').click(function() {
     vendor: "joe",
   };
 
-  var permissions = {
-    '_public': [
-      'Read',
-      'Insert'
-    ]
-  };
-
-  var owncontainer = {
-    own_container: true
-  };
 
   window.safeApp.initialise(app)
     .then((appHandle) => {
       console.log("Initialise Token: " + appHandle);
-      window.safeApp.authorise(appHandle, permissions, owncontainer)
-        .then((authURI) => {
-          // console.log(auth);
-          window.safeApp.connectAuthorised(appHandle, authURI)
-            .then((authorisedAppHandle) => {
-              //returns authorised app token
-              auth = authorisedAppHandle;
-              Materialize.toast("Authorised App Token: " + auth, 3000, 'rounded');
-              getMessages();
-
-              // console.log(authorisedAppHandle);
-            });
+      window.safeApp.connect(appHandle)
+        .then((appHandle) => {
+          //returns app token
+          auth = appHandle;
+          authorised = false;
+          Materialize.toast(" App Token: " + auth, 3000, 'rounded');
+          getMutableDataHandle();
         });
     }, (err) => {
       console.error(err);
-      // Materialize.toast(err, 3000, 'rounded');
+      Materialize.toast(err, 3000, 'rounded');
     });
 })();
 
-function getMessages() {
+function authorise() {
+  if (authorised === false){
+  window.safeApp.free(auth);
+  auth = "";
+    var app = {
+      name: "Safe Chat",
+      id: "joe",
+      version: "1",
+      vendor: "joe",
+    };
+
+    var permissions = {
+      '_public': [
+        'Read',
+        'Insert'
+      ]
+    };
+
+    var owncontainer = {
+      own_container: true
+    };
+
+    window.safeApp.initialise(app)
+      .then((appHandle) => {
+        console.log("Initialise Token: " + appHandle);
+        window.safeApp.authorise(appHandle, permissions, owncontainer)
+          .then((authURI) => {
+            // console.log(auth);
+            window.safeApp.connectAuthorised(appHandle, authURI)
+              .then((authorisedAppHandle) => {
+                //returns authorised app token
+                auth = authorisedAppHandle;
+                authorised = true;
+                Materialize.toast("Authorised App Token: " + auth, 3000, 'rounded');
+                getMutableDataHandle();
+                sendMessage();
+              });
+          });
+      }, (err) => {
+        console.error(err);
+        Materialize.toast(err, 3000, 'rounded');
+      });
+    } else {
+      sendMessage();
+    }
+  }
+
+function getMutableDataHandle() {
   var name = "safechat";
   window.safeCrypto.sha3Hash(auth, name)
-    .then((hash) => {
-      window.safeMutableData.newPublic(auth, hash, 3000)
-        .then((mdHandle) => {
-          window.safeMutableData.getEntries(mdHandle)
-            .then((entriesHandle) => {
-              messages.innerHTML = "";
-              window.safeMutableDataEntries.forEach(entriesHandle,
-                (key, value) => {
-                  console.log('Key: ', uintToString(key));
-                  console.log('Value: ', uintToString(value.buf));
-                  $("#messages").append('<div class="row"><div class="card-panel yellow"><span class="blue-text">' + uintToString(value.buf) + '</span></div></div>');
-                    window.scrollTo(0,document.body.scrollHeight);
-                });
-            });
+    .then((hash) =>
+      window.safeMutableData.newPublic(auth, hash, 3000))
+    .then((safeChatHandle) => {
+      mdHandle = safeChatHandle;
+      getMessages();
+    });
+}
+
+function getMessages() {
+  window.safeMutableData.getEntries(mdHandle)
+    .then((entriesHandle) => {
+      messages.innerHTML = "";
+      window.safeMutableDataEntries.forEach(entriesHandle,
+        (key, value) => {
+          console.log('Key: ', uintToString(key));
+          console.log('Value: ', uintToString(value.buf));
+          $("#messages").append('<div class="row"><div class="card-panel yellow"><span class="blue-text">' + uintToString(value.buf) + '</span></div></div>');
+          window.scrollTo(0, document.body.scrollHeight);
         });
     }, (err) => {
       console.error(err);
@@ -74,33 +110,28 @@ function getMessages() {
     });
 }
 
-function sendmessage() {
-  var name = "safechat";
-  window.safeCrypto.sha3Hash(auth, name)
-    .then((hash) => {
-      window.safeMutableData.newPublic(auth, hash, 3000)
-        .then((mdHandle) => {
-          window.safeMutableData.newMutation(auth)
-            .then((mutationHandle) => {
-              var date = new Date();
-              var time = date.getTime();
-              window.safeMutableDataMutation.insert(mutationHandle, time.toString(), textarea.value)
-                .then(_ =>
-                  window.safeMutableData.applyEntriesMutation(mdHandle, mutationHandle))
-                .then(_ =>
-                  Materialize.toast('Message has been sent to the network, you might need to click the refresh button to see it', 3000, 'rounded'));
-              textarea.value = "";
-              getMessages();
-            });
+function sendMessage() {
+  window.safeMutableData.newMutation(auth)
+    .then((mutationHandle) => {
+      var date = new Date();
+      var time = date.getTime();
+      window.safeMutableDataMutation.insert(mutationHandle, time.toString(), textarea.value)
+        .then(_ =>
+          window.safeMutableData.applyEntriesMutation(mdHandle, mutationHandle))
+        .then(_ => {
+          Materialize.toast('Message has been sent to the network', 3000, 'rounded');
+          getMessages();
         });
+      textarea.value = "";
+
     });
 }
 
 function uintToString(uintArray) {
   return new TextDecoder("utf-8").decode(uintArray)
-          .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
